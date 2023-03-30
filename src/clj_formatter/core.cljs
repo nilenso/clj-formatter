06199a612fba01768f2e20cf17186c29a167aa60
Shivam <shivam@nilenso.com>
2023-03-30 13:22:34 +0530

    Add function to get unformatted code from textarea

--- src/clj_formatter/core.cljs
+++ src/clj_formatter/core.cljs
@@ -6,5 +6,17 @@
 ;;   (repl/connect "http://localhost:9000/repl"))
 
 
+;; (defn debug [elem]
+;;   (js/console.log elem)
+;;   elem)
+
+(defn get-input-value [dom-query]
+  (let [elem (js/document.querySelector dom-query)]
+    (.-value elem)))
+
+(defn handle-on-format [_]
+  (-> "#unformatted-code-area"
+      get-input-value))
+
 (let [el (js/document.getElementById "submit-unformatted-code")]
   (.addEventListener el "click" handle-on-format))
